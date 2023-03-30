8ddaa2901fe957cded4757464e9f43525079cb2e
dartx <castete>
2023-03-30 17:54:49 -0500

    correctly port

--- app.js
+++ app.js
@@ -105,6 +105,6 @@ app.get('/cotizaciones/:id', async (req, res) => {
     // });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Backend listening on port 3000!');
 });
